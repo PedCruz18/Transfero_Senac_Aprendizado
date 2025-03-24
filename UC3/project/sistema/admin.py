0cf4458ac9e9b0f5188b9171a4387b6418fe8b16
PedCruz18 <Pedroemanueldasilvacruz@gmail.com>
2025-03-24 16:51:51 -0300

    1.3

--- UC3/project/sistema/admin.py
+++ UC3/project/sistema/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from sistema import models
 
-# Register your models here.
+@admin.register(models.Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+ list_display = ('id', 'nome', 'sobrenome', 'email', 'ativo',)
+ 
--- UC3/project/sistema/models.py
+++ UC3/project/sistema/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Usuario(models.Model):
+ nome = models.CharField(max_length=50)
+ sobrenome = models.CharField(max_length=50)
+ cpf = models.CharField(max_length=11)
+ telefone = models.CharField(max_length=20)
+ email = models.EmailField()
+ endereco = models.CharField(max_length=100)
+ data_cadastro = models.DateTimeField(default=timezone.now)
+ ativo = models.BooleanField(default=True)
+ # foto = models.ImageField(upload_to='fotos/', blank=True, null=True)  # Exemplo de campo para foto
